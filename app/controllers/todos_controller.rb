b5496aafdb389ec328a932ab97abd63ca9843fdd
yuuuba <nsabu45@gmail.com>
2021-11-27 18:45:50 +0900

    destroyまで

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,32 @@
 class TodosController < ApplicationController
   def index
     @todos = Todo.all
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.create(todo_params)
+    redirect_to todos_path
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update(todo_params)
+    redirect_to todos_path
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    redirect_to todos_path
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:title)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get '/todos', to: 'todos#index', as: 'todos'
   post '/todos', to: 'todos#create'
+  get '/todos/:id/edit', to: 'todos#edit', as: 'edit_todo'
+  patch '/todos/:id', to: 'todos#update'
+  delete '/todos/:id', to: 'todos#destroy'
 end
